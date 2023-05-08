549e4ac43d9532de3bdf2ad0d2e858496a6dfef3
Lech Piechota <lech.piechota@ocado.com>
2023-05-08 11:29:09 +0200

    add unit tests for CitiHtmlTransactionParser and refactor

--- src/main/kotlin/com/lpi/fdt/experimental/htmlparser/CitiHtmlTransactionParser.kt
+++ src/main/kotlin/com/lpi/fdt/experimental/htmlparser/CitiHtmlTransactionParser.kt
@@ -1,31 +1,37 @@
 package com.lpi.fdt.experimental.htmlparser
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class CitiHtmlTransactionParser : HtmlTransactionParser {
-    override fun parseTransactions(html: String): List<BudgetTransaction> {
-        val doc: Document = Jsoup.parse(html)
+    override fun parseTransactions(html: String): List<BudgetTransaction> =
+        getTransactionElements(html)
+            .mapNotNull { it.parseSingleTransaction() }
+            .sortedBy { it.date }
 
-        val transactionElements = doc.select("div[data-index]")
+    private fun getTransactionElements(html: String) =
+        Jsoup.parse(html).select("div[data-index]")
 
-        return transactionElements.map { transactionElement ->
-            val dateElement = transactionElement.selectFirst("span[id^=transactionDate_]")
-            val descriptionElement = transactionElement.selectFirst(".cbol-trans-desc")
-            val amountElement = transactionElement.selectFirst(".format-amount-holder")
+    private fun Element.parseSingleTransaction(): BudgetTransaction? {
+        val dateElement = selectFirst("span[id^=transactionDate_]")
+        val descriptionElement = selectFirst(".cbol-trans-desc")
+        val amountElement = selectFirst(".format-amount-holder")
 
-            val rawDate = dateElement!!.text()
+        val rawDate = dateElement?.text()
+        val rawAmount = amountElement?.text()
+
+        return if (rawDate != null && rawAmount != null) {
             val dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale("pl", "PL"))
-            val parsedDate = LocalDate.parse(rawDate, dateFormatter)
-            val description = descriptionElement!!.text()
-            val amount = BigDecimal(amountElement!!.text().replace(",", ".").replace(" ", ""))
+            val description = descriptionElement?.text() ?: "-- DESCRIPTION NOT PROVIDED --"
+            val amount = BigDecimal(rawAmount.replace(",", ".").replace(" ", ""))
 
-            BudgetTransaction(parsedDate, amount, description)
-        }.sortedBy { it.date }
+            val parsedDate = LocalDate.parse(rawDate, dateFormatter)
+            BudgetTransaction(parsedDate, -amount, description)
+        } else null
     }
 
 }
